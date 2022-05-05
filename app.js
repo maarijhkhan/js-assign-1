dbb0e3d88006a0abd9630edcea7d544370649021
Five Star Computer <maarijhkhan@gmail.com>
2022-05-05 14:37:40 -0700

    Work Complete

---
+++ app.js
@@ -0,0 +1,381 @@
+//chapter 1 question no: 1
+//alert("Thanks for visiting!");
+
+
+//chapter 1 question no: 2
+//alert("first page");
+
+//chapter 1 question no: 3
+
+//var a="Welcome to JS Land \n Happy Coding";
+//alert(a);
+
+
+//chapter 1 question no: 4
+
+//chapter 1 question no: 5
+//console.log("Hello I can run js through  my web browser's console");
+
+//question no 6
+//alert("Welcome");
+
+//question no 7 in index.html
+
+
+//chapter 2 question no: 1
+//var username;
+
+//chapter 2 question no: 2
+
+//var myName = "Maarij Khan";
+//document.write(myName);
+
+//chapter 2 question no 3
+//var message;
+//message="Hello World";
+//alert(message);
+
+//chapter 2 question no 4
+/*var name="John Doe";
+var age="15 years old";
+var qual = "certified mobile application development";
+alert(name);
+alert(age);
+alert(qual);
+*/
+
+//chapter 2 question no: 5
+//var p = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+//alert(p)
+
+//chapter 2 question no: 6
+//var email = "My email address is maarijhkhan@gmail.com";
+//alert(email);
+
+//chapter 2 question no: 7
+//var book="A smarter way to learn Javascript";
+//alert("I am trying to learn from the Book "+book);
+
+//chapter 2 question no: 8
+//document.write("Yah! I can write HTML content through Javascript");
+
+//chapter 2 question no: 9
+//var a="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+//alert(a);
+
+//chapter 3 question no 1
+//var age = 20;
+//alert("I am " + age + " years old");
+
+//chapter 3 question no 2
+//var n;
+//n=15;
+//alert("You have visited this site " + n + " times");
+
+
+//chapter 3 question no 3
+//var birthYear;
+//birthYear=2001;
+//document.write("My birthYear is "+ birthYear + "<br>"+" Data type of my declared variable is number ");
+
+
+//chapter 3 question no 4
+//var Name="khadija";
+//var product_title = "t_shirt";
+//var quantity = 5;
+//document.write(Name + " ordered " + quantity + product_title + " on XYZ Clothing Store " );
+
+//end of chp3
+
+//chapter 4 question no 1
+//var Name ; age ; Message; 
+
+//chapter 4 question no 2
+
+//llegal variable names
+//var x; y; z; Cartoon; quantity;
+//illegal variable names
+//var -name; alert; 9ap; 10$chips; print;
+
+//chapter 4 question no 3
+//var heading = "Rules for naming JS variables";
+//document.write(heading.bold());
+//var a = "Variable names can only contain numbers,$ and _ For example: $my_1stVariable";
+//document.write("<br>"+"<br>"+"<br>"+ a); 
+//var b = "Variable must begin with a letter, $ or _. For example: $name, _name or name";
+//document.write("<br>"+b);
+//var c= "Variable names are case sensitive";
+//document.write("<br>"+c);
+//var d = "Variable names should not be JS Keywords";
+//document.write("<br>"+d);
+
+
+//chapters 6-9 questions
+//question no 3
+
+//var name = window.prompt("Enter your name");
+//alert("Hello " + name);
+
+
+//question no 5
+//var number = window.prompt("Enter a number");
+//for(let i =1; i<=10; i++){
+  //  var result = i*number;
+    //document.write(number + " * " + i + result);
+    //console.log(`${number} * ${i} = ${result}`);
+//}
+
+//question no 6
+/*
+var subject_1 = window.prompt("Enter subj1 name");
+var subject_2 = window.prompt("Enter subj2 name");
+var subject_3 = window.prompt("Enter subj3 name");
+var s1total_marks = 100;
+var s2total_marks = 100;
+var s3total_marks = 100;
+var obtained_markss1 = parseInt(window.prompt("Obtained marks of subj1 "));
+var obtained_markss2 = parseInt(window.prompt("Obtained marks of subj2 "));
+var obtained_markss3 = parseInt(window.prompt("Obtained marks of subj3 "));
+var total_marks = s1total_marks + s2total_marks + s3total_marks;
+var Ototal_marks = obtained_markss1 + obtained_markss2 + obtained_markss3;
+var percentage = Ototal_marks/total_marks*100;
+
+document.write("Subject"+'&nbsp &nbsp'+"Total Marks"+'&nbsp &nbsp'+"Obtained Marks"+'&nbsp &nbsp'+"Percentage"+"<br>");
+
+document.write(subject_1+'&nbsp &nbsp &nbsp  &nbsp &nbsp'+ s1total_marks +'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+ obtained_markss1 +
+ '&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  '  + obtained_markss1+"%"+"<br>" );
+
+ document.write(subject_2+'&nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp'+ s2total_marks +'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+ obtained_markss2 +
+ '&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp '  + obtained_markss2+"%"+"<br>" );
+
+ document.write(subject_3+'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+ s3total_marks +'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+ obtained_markss3 +
+ '&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp '  + obtained_markss3+"%"+"<br>" );
+
+ document.write("<br>"+"<br>"+"<br>"+'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp'+ total_marks +'&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp' + Ototal_marks +
+  '&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp' + percentage);
+
+*/
+
+//question 2 of chp 6-9
+//var a =2;
+//var b= 1;
+//--a;
+// The output is 1 
+//The value of a is 2 after predecreement(--a) tha value of a become 1 because first it decreement the value then print it.
+
+//--a - --b;
+// The output is 1
+//because in the preincreement of a bacome 1 after 2 and b value become 0 after 1 and when we subtract both the values 
+//it become 1
+
+//--a - --b + ++b;
+//The output is 2
+// var a value become 1 after the predecreement and var b value become 0 after b decreement then after the preincrement 
+//the var b value become 1 so the output is 2
+
+//--a - --b + ++b + b--;
+//The output is 3
+//because after the predecreement of var a the value of var a become 1 and then we subtract with the predecreement value of var b
+//which is 0 and then adding the value with the post decreement value odf var b which become 1.
+
+
+
+//question 1 of chapter 6-9
+//var a= 10;
+//document.write("Result:"+"<br>");
+//document.write("The value of a is:"+a+"<br>");
+//document.write("*****************"+"<br>"+"<br>");
+//var f = ++a;
+//document.write("The value of ++a is:" + f + "<br>" );
+//document.write("Now the value of a is:"+f+"<br>");
+//document.write("<br><br><br>");
+
+//var f = a++
+//document.write("The value of a++ is:"+f+"<br>");
+//var f = ++a ;
+//document.write(" Now the value of a is:"+f +"<br>");
+//document.write("<br><br><br>");
+
+//var f= --a;
+//document.write("The value of --a is:" +f+"<br>");
+//document.write("Now the value of a is:"+f+"<br>");
+
+//document.write("<br><br><br>");
+//document.write("The value of a-- is:"+f+"<br>");
+//var f = --a;
+//document.write("Now the value of a is:"+f);
+
+
+//chapter 5 question no 1
+
+//var a = parseInt(prompt("Enter the first no:"));
+//var b = parseInt(prompt("Enter the first no:"));
+//var c = a+b;
+//document.write("Sum of " + a + " and " + b + " is " + c); 
+
+//chapter 5 question no 2
+
+//subtraction
+
+//var a = parseInt(prompt("Enter the first no:"));
+//var b = parseInt(prompt("Enter the first no:"));
+//var c = a-b;
+//document.write("Subtraction of " + a + " and " + b + " is " + c);
+ 
+//Multiplication
+
+//var a = parseInt(prompt("Enter the first no:"));
+//var b = parseInt(prompt("Enter the first no:"));
+//var c = a*b;
+//document.write("Multiplication of " + a + " and " + b + " is " + c); 
+
+//Modulus
+
+//var a = parseInt(prompt("Enter the first no:"));
+//var b = parseInt(prompt("Enter the first no:"));
+//var c = a%b;
+//document.write("Modulus of " + a + " and " + b + " is " + c); 
+
+//chapter 5 question no: 3
+
+//var a;
+//document.write("Value after variable declaration is undefined"+"<br>");
+//a=5;
+//document.write("Initial value: "+ a +"<br>");
+//var c = ++a;
+//document.write("Value after increment is: " + c + "<br>");
+//var d = c + 7;
+//document.write("Value after addition is: " + d + "<br>");
+//var e = --d;
+//document.write("Value after decrement is: " + e + "<br>");
+//var f = e%3;
+//document.write("The remainder is: " + f);
+
+
+//chapter 5 question no: 4
+//var a;
+//a=600;
+//var b;
+//b= 5;
+//var t=a*b;
+//document.write("Total cost to buy " + b + " tickets to a movie is " + t + "PKR");
+
+//chapter 5 question no: 6
+//var a = parseInt(prompt("Temp in celcius:"));
+//var c = (a*9/5)+32;
+//document.write(a+" is " + c +"F"+"<br>");
+//var b = parseInt(prompt("Temp in ferenheit:"));
+//var f = (b-32)*5/9;
+//document.write(b+ " is " + f + "C");
+
+
+//chapter 5 question no: 8
+
+//var heading = "Marks Sheet";
+//document.write(heading.bold()+"<br><br><br><br>");
+//var a = 980;
+//document.write("Total marks "+ a + "<br>");
+//var b = parseInt(prompt("Obtained marks:"));
+//document.write("Obtained marks: "+ b + "<br>");
+//var c = b/a*100;
+//document.write("Percentage: "+ c);
+
+//chapter 5 question no 5
+/*document.write("Table of 4"+"<br>");
+var num=4;
+for(i=1; i<11; i++){
+  var result = num*i;
+  document.write("<br>"+"4 x "+i+"="+result);
+}
+*/
+//chapter 5 question no 7
+
+/*var msg="SHOPPING CART";
+document.write(msg.bold()+"<br><br><br>");
+var Price_of_item1;
+var Price_of_item2;
+var Ordered_quantity_of_item1;
+var Ordered_quantity_of_item2;
+var Shipping_charges;
+
+Price_of_item1=parseInt(prompt("item 1 price"));
+Ordered_quantity_of_item1=parseInt(prompt("quantity of item 1"));
+Price_of_item2=parseInt(prompt("item 2 price"));
+Ordered_quantity_of_item2=parseInt(prompt("quantity of item 2"));
+Shipping_charges=parseInt(prompt("shipping charges"));
+
+var Total_cost_of_your_order_is = Price_of_item1*Ordered_quantity_of_item1+Price_of_item2*Ordered_quantity_of_item2+Shipping_charges;
+
+document.write("Price_of_item1 "+Price_of_item1+"<br>");
+document.write("Ordered quantity of item1 "+Ordered_quantity_of_item1+"<br>");
+document.write("Price_of_item2 "+Price_of_item2+"<br>");
+document.write("Ordered quantity of item2 "+Ordered_quantity_of_item2+"<br>");
+document.write("Shipping Charges "+Shipping_charges+"<br>");
+document.write("Total cost of your order is "+Total_cost_of_your_order_is+"<br>");
+*/
+
+//chapter 5 question no 12
+
+/*
+var msg="The Geometrizer";
+document.write(msg.bold()+"<br><br>");
+var r=parseInt(prompt("Enter the radius"));
+var pi=3.142;
+var Circumference_of_a_circle=2*pi*r;
+var Circumference_of_a_circle1=2*pi*r*10;
+document.write("Radius of a circle: "+r+"<br>");
+document.write("The Circumference is:" +Circumference_of_a_circle+"<br>");
+document.write("The area is: "+Circumference_of_a_circle1);
+*/
+
+//chapter 5 question no 11
+
+/*
+var msg="Age Calculator";
+document.write(msg.bold()+"<br><br>");
+var Current_year=parseInt(prompt("Current year"));
+var Birth_year=parseInt(prompt("Birth year"));
+var age=Current_year-Birth_year;
+document.write("Current Year: " + Current_year+"<br>");
+document.write("Birth Year: " + Birth_year+"<br>");
+document.write("Your Age: " + age);
+*/
+
+
+//chapter 5 question no 10
+/*
+var num=5;
+var calc=num+5*10/2;
+alert(calc);
+*/
+
+//chapter 5 question no 9
+
+/*var msg="Currency in PKR";
+document.write(msg.bold()+"<br><br>");
+var dollars=parseInt(prompt("Enter dollars"));
+var riyal=parseInt(prompt("Enter riyal"));
+var us_dollars=dollars*104.80;
+var saudi_riyal=riyal*28;
+var tcurrency= us_dollars+saudi_riyal;
+document.write("Total Currency in PKR: "+tcurrency);
+*/
+
+
+//chapter 5 question no 13
+
+/*var msg="The LifeTime Supply Calculator";
+document.write(msg.bold()+"<br><br>");
+var s=prompt("Enter your snack");
+var a=parseInt(prompt("Enter your current age"));
+var e =parseInt(prompt("Estimated max age"));
+var d =parseInt(prompt("Amount of Snacks per day"));
+var c=e-a;
+var c1=c*d;
+document.write("Favourite Snake: "+s+"<br>");
+document.write("Current age: "+a+"<br>");
+document.write("Estimated maximum age: "+e+"<br>");
+document.write("Amount of Snacks per day: "+d+"<br>");
+document.write("You will need "+c1+" Chocolate chip to last you until the ripe old age of "+e);
+
+*/
